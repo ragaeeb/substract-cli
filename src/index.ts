ed8596ffa13840ded9f22e9a69e368a0297ea33b
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-11-15 13:28:30 +0400

    Support twitter/x video processing #3

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import logSymbols from 'log-symbols';
 import { substract } from 'substract';
 
 import { name, version } from './utils/constants.js';
+import logger from './utils/logger.js';
 import { mapFileOrUrlToInputSource, mapFlagsToOptions } from './utils/optionsMapping.js';
 import { getCliArgs } from './utils/prompt.js';
 
@@ -19,13 +20,19 @@ const main = async () => {
     const cli = getCliArgs();
     const options = mapFlagsToOptions(cli);
     const inputSource = await mapFileOrUrlToInputSource(cli.input);
+
+    if (!inputSource) {
+        logger.warn(`${logSymbols.error} No video found at ${cli.input}`);
+        return;
+    }
+
     const result = await substract(inputSource, options);
 
-    if (result) {
-        console.log(`${logSymbols.success} written ${result}`);
-    } else {
-        console.warn(`${logSymbols.error} Nothing written`);
+    if (!result) {
+        logger.warn(`${logSymbols.error} Nothing written`);
     }
+
+    logger.info(`${logSymbols.success} written ${result}`);
 };
 
 main();
